49329be828ddcc3ca63143fa232f96293bd4cd15
thomasskull666 <redingp@gmail.com>
2011-06-12 20:09:54 -0500

    initramfs from Clean Kernel by DRockstar, thanks

--- initramfs/sbin/custom_init.sh
+++ initramfs/sbin/custom_init.sh
@@ -1,107 +1,90 @@
 #!/system/bin/sh
-# Originally written by tanimn for Twilight Zone Kernel 1.0.3
-# Modified by Rodderik for Genocide Kernel
+# SCH-R910 custom init script, based on DRockstar Clean Kernel sdx.sh script called from init.rc
 
-# Do Customizations
+/sbin/busybox mount -o remount,rw /dev/block/stl10 /system
+/sbin/busybox mount -o remount,rw / /
 
-# Remount filesystems RW
-
-busybox mount -o remount,rw /
-busybox mount -o remount,rw /system
-
-# Install BusyBox 1.18
-
-/sbin/busybox --install -s /system/bin
+# Install busybox
+/sbin/busybox mkdir /bin
+/sbin/busybox2 rm -rf /sbin/busybox
+/sbin/busybox2 mv /sbin/busybox2 /sbin/busybox
+/sbin/busybox --install -s /bin
+rm -rf /bin/ls
+rm -rf /system/xbin/busybox
+ln -s /sbin/busybox /system/xbin/busybox
+rm -rf /sbin/recovery
+rm -rf /res/sbin
 sync
 
-# Lock the CPU down til SetCPU or such can set it.
-
-echo 1000000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-echo 1000000 > /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq
-
-# Switch to Conservative CPU governor after bootup
-
-echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-
-# Destory 20-scheduler, put in the new one for BFQ by default
-
-rm -rf /system/etc/init.d/20-scheduler
-busybox cp /sbin/20-scheduler /system/etc/init.d/20-scheduler
-
-# Enable init.d support
-
-if [ -d /system/etc/init.d ]
-then
-	logwrapper busybox run-parts /system/etc/init.d
-fi
-sync
-
-# keyboard patch sysfs call 5 for snappy keyboard performance (range: 1-16 default: 7)
-# Thanks DRockstar
-
-if [ ! -f "/data/local/timer_delay" ]; then
-  echo 5 > /data/local/timer_delay
-fi
-cat /data/local/timer_delay > /sys/devices/platform/s3c-keypad/timer_delay
+# Fix permissions in /sbin, just in case
+chmod 755 /sbin/*
 
 # Fix screwy ownerships
-
 for blip in conf default.prop fota.rc init init.goldfish.rc init.rc init.smdkc110.rc lib lpm.rc modules recovery.rc res sbin
 do
-	chown root.system /$blip
-	chown root.system /$blip/*
+  chown root.system /$blip
+  chown root.system /$blip/*
 done
 
 chown root.system /lib/modules/*
 chown root.system /res/images/*
 
-
-chmod 6755 /sbin/su
-rm /system/bin/su
-rm /system/xbin/su
-ln -s /sbin/su /system/bin/su
-ln -s /sbin/su /system/xbin/su
-
-#setup proper passwd and group files for 3rd party root access
-# Thanks DevinXtreme
-
-if [ ! -f "/system/etc/passwd" ]; then
-	echo "root::0:0:root:/data/local:/system/bin/sh" > /system/etc/passwd
-	chmod 0666 /system/etc/passwd
+# Enable init.d support
+if [ -d /system/etc/init.d ]
+then
+  logwrapper busybox run-parts /system/etc/init.d
 fi
-if [ ! -f "/system/etc/group" ]; then
-	echo "root::0:" > /system/etc/group
-	chmod 0666 /system/etc/group
-fi
-
-#Check and setup Superuser if missing
-
- if [ ! -f "/system/app/Superuser.apk" ] && [ ! -f "/data/app/Superuser.apk" ] && [[ ! -f "/data/app/com.noshufou.android.su"* ]]; then
-	if [ -f "/system/app/Asphalt5_DEMO_ANMP_Samsung_D700_Sprint_ML.apk" ]; then
-		rm /system/app/Asphalt5_DEMO_ANMP_Samsung_D700_Sprint_ML.apk
-	fi
-	if [ -f "/system/app/FreeHDGameDemos.apk" ]; then
-		rm /system/app/FreeHDGameDemos.apk
-	fi
- 	busybox cp /support/Superuser.apk /system/app/Superuser.apk
- fi
-
-# fix busybox DNS while system is read-write
-
-if [ ! -f "/system/etc/resolv.conf" ]; then
-	echo "nameserver 8.8.8.8" >> /system/etc/resolv.conf
-	echo "nameserver 8.8.4.4" >> /system/etc/resolv.conf
-fi 
 sync
 
-# Patch to attempt removal and prevention of DroidDream malware
-if [ -f "/system/bin/profile" ]; then
-	rm /system/bin/profile
+# setup tmpfs for su
+mkdir /sdx
+mount -o size=30k -t tmpfs tmpfs /sdx
+cat /sbin/su > /sdx/su
+chmod 06755 /sdx/su
+# establish root in common system directories for 3rd party applications
+rm /system/bin/su
+rm /system/xbin/su
+rm /system/bin/jk-su
+ln -s /sdx/su /system/bin/su
+ln -s /sdx/su /system/xbin/su
+# remove su in problematic locations
+rm -rf /bin/su
+rm -rf /sbin/su
+
+# fix busybox DNS while system is read-write
+if [ ! -f "/system/etc/resolv.conf" ]; then
+  echo "nameserver 8.8.8.8" >> /system/etc/resolv.conf
+  echo "nameserver 8.8.4.4" >> /system/etc/resolv.conf
 fi
-touch /system/bin/profile
+
+# setup proper passwd and group files for 3rd party root access
+if [ ! -f "/system/etc/passwd" ]; then
+  echo "root::0:0:root:/data/local:/system/bin/sh" > /system/etc/passwd
+  chmod 0666 /system/etc/passwd
+fi
+if [ ! -f "/system/etc/group" ]; then
+  echo "root::0:" > /system/etc/group
+  chmod 0666 /system/etc/group
+fi
+
+# provide support for a shell script to protect root access
+if [ ! -f "/system/app/Superuser.apk" ]; then
+  cp /sbin/Superuser.apk /system/app/Superuser.apk
+fi
+rm /sbin/Superuser.apk
+
+# patch to prevent certain malware apps
+. > /system/bin/profile
 chmod 644 /system/bin/profile
 
-# remount read only and continue
-busybox mount -o remount,ro /
-busybox mount -o remount,ro /system
+# keyboard patch sysfs call 7 for snappy keyboard performance
+if [ ! -f "/data/local/timer_delay" ]; then
+  echo 7 > /data/local/timer_delay
+fi
+cat /data/local/timer_delay > /sys/devices/platform/s3c-keypad/timer_delay
+
+sync
+
+mount -o remount,ro /dev/stl9 /system
+mount -o remount,ro / /
 
