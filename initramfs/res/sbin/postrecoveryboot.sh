cbc0e3af8c1710e68fadae13abaccb3e461dbca3
thomasskull666 <redingp@gmail.com>
2011-06-16 21:15:15 -0500

    new initramfs, other stuff

--- initramfs/res/sbin/postrecoveryboot.sh
+++ initramfs/res/sbin/postrecoveryboot.sh
@@ -1,12 +1,6 @@
 #!/sbin/sh
 
 # Restart with root hacked adbd
-echo msc_adb > /dev/usb_device_mode
-touch /tmp/recovery.log
-sync
-
-NEEDS_ADBD=$(ps | grep adbd | grep -v grep)
-if [ -z "$NEEDS_ADBD" ]
-then
-    /sbin/adbd recovery &
-fi
+cat /proc/kmsg
+ls -l /dev/block
+ps
